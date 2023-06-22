41439b1124f3cb3e22e4516edeb6ac4630c6e230
eduard-kaluhin <renoserviser@gmail.com>
2023-06-22 21:28:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,73 +1,89 @@
-import {Notify} from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import {findImages} from './api';
+import { findImages } from './api';
 
-const galleryEl = document.querySelector ('.gallery');
-const fromEl = document.querySelector ('#search-form');
-const buttonEl = document.querySelector ('.load-more');
-const inputEl = document.querySelector ('input');
+const galleryEl = document.querySelector('.gallery');
+const fromEl = document.querySelector('#search-form');
+const buttonEl = document.querySelector('.load-more');
+const inputEl = document.querySelector('input');
 let nameImages = '';
 let page = 0;
 let perPage = 40;
-fromEl.addEventListener ('submit', create);
-buttonEl.addEventListener ('click', loadMore);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+  disableScroll: false,
+});
 
-function create (e) {
+fromEl.addEventListener('submit', create);
+buttonEl.addEventListener('click', loadMore);
+
+function create(e) {
   nameImages = '';
   page = 0;
   galleryEl.innerHTML = '';
-  buttonEl.classList.remove ('block');
-  e.preventDefault ();
-  if ((e.action = inputEl.value === '')) {
-    return Notify.failure (
+  buttonEl.style.display = 'none'; // Приховуємо кнопку "Load more"
+  e.preventDefault();
+
+  nameImages = inputEl.value.trim();
+  if (nameImages === '') {
+    return Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-  nameImages = e.action = inputEl.value;
-  page += 1;
-  findImages (nameImages, perPage, page)
-    .then (res => {
+
+  page = 1;
+  findImages(nameImages, perPage, page)
+    .then((res) => {
       if (!res.data.hits.length > 0) {
-        Notify.failure (
+        Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
-        return createCards (res), buttonEl.classList.add (
-          'block'
-        ), (buttonEl.disabled = false);
+        createCards(res);
+        buttonEl.classList.remove('hidden'); // Показуємо кнопку "Load more"
+        buttonEl.disabled = false;
+        hidingBtnLoadMore(res.data.totalHits);
       }
     })
-    .catch (error => {
-      Notify.failure (
-        'Sorry, there are no images matching your search query. Please try again.'
+    .catch((error) => {
+      Notify.failure(
+        'Sorry, there was an error while fetching images. Please try again.'
       );
     });
 }
 
-function loadMore (e) {
-  e.preventDefault ();
-  page += 1;
-  findImages (nameImages, perPage, page)
-    .then (res => {
+function loadMore(e) {
+  e.preventDefault();
+  buttonEl.style.display = 'none';
+  page++;
+  findImages(nameImages, perPage, page)
+    .then((res) => {
       if (!res.data.hits.length > 0) {
-        Notify.failure (
-          'Sorry, there are no images matching your search query. Please try again.'
+       
+        Notify.failure(
+          'Sorry, there are no more images matching your search query.'
         );
       } else {
-        return createCards (res), hidingBtnLoadMore (res.data.totalHits);
+        
+        createCards(res);
+        hidingBtnLoadMore(res.data.totalHits);
+        lightbox.refresh();
       }
     })
-    .catch (error => {
-      Notify.failure (
-        'Sorry, there are no images matching your search query. Please try again.'
+    .catch((error) => {
+      Notify.failure(
+        'Sorry, there was an error while fetching images. Please try again.'
       );
     });
 }
-function createCards (arr) {
+
+function createCards(arr) {
+    buttonEl.style.display = 'block';
   const object = arr.data.hits;
   const card = object
-    .map (
+    .map(
       ({
         webformatURL,
         largeImageURL,
@@ -78,48 +94,39 @@ function createCards (arr) {
         downloads,
       }) => {
         return ` <div class="photo-card">
-   <a class="gallery-link" href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" height="250"/>
-    </a>
-    <div class="info">
-    <p class="info-item">
-    <b>Likes</b>
-    ${likes}
-    </p>
-    <p class="info-item">
-    <b>Views</b>
-    ${views}
-    </p>
-    <p class="info-item">
-    <b>Comments</b>
-    ${comments}
-    </p>
-    <p class="info-item">
-    <b>Downloads</b>
-    ${downloads}
-    </p>
-    </div>
-    </div> `;
+          <a class="gallery-link" href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="250" />
+          </a>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              ${downloads}
+            </p>
+          </div>
+        </div> `;
       }
     )
-    .join ('');
-  return galleryEl.insertAdjacentHTML (
-    'beforeend',
-    card
-  ), new SimpleLightbox ('.gallery a', {
-    captionsData: 'alt',
-    captionsDelay: 250,
-    disableScroll: false,
-  }).refresh ();
-}
+    .join('');
 
-function hidingBtnLoadMore (total) {
-  let comparison = page * perPage < total;
-  if (!comparison) {
-    (buttonEl.disabled = true), Notify.failure (
-      "We're sorry, but you've reached the end of search results."
-    );
-  } else {
-    return;
-  }
+  galleryEl.insertAdjacentHTML('beforeend', card);
 }
+function hidingBtnLoadMore(total) {
+    const comparison = page * perPage < total;
+    if (!comparison) {
+      buttonEl.style.display = 'none'; // Приховуємо кнопку "Load more";
+    } else {
+      buttonEl.style.display = 'block'; // Показуємо кнопку "Load more"
+    }
+  }
